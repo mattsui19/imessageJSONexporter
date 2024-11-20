0a7f3795b7baae40f63be3777cb4dfd61984eeee
Christopher Sardegna <github@reagentx.net>
2024-11-19 16:27:21 -0800

    Update tests to use `BTreeSet`

--- imessage-database/src/tables/messages/tests/query_tests.rs
+++ imessage-database/src/tables/messages/tests/query_tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use std::env::set_var;
+    use std::{collections::BTreeSet, env::set_var};
 
     use crate::{tables::messages::Message, util::query_context::QueryContext};
 
@@ -53,12 +53,12 @@ mod tests {
     #[test]
     fn can_generate_filter_statement_chat_ids() {
         let mut context = QueryContext::default();
-        context.set_selected_handle_ids(vec![1, 2, 3]);
+        context.set_selected_chat_ids(BTreeSet::from([1, 2, 3]));
 
         let statement = Message::generate_filter_statement(&context);
         assert_eq!(
             statement,
-            " WHERE\n                     m.chat_id IN (1, 2, 3)"
+            " WHERE\n                     c.chat_id IN (1, 2, 3)"
         )
     }
 
@@ -67,10 +67,10 @@ mod tests {
         let mut context = QueryContext::default();
         context.set_start("2020-01-01").unwrap();
         context.set_end("2020-02-02").unwrap();
-        context.set_selected_handle_ids(vec![1, 2, 3]);
+        context.set_selected_chat_ids(BTreeSet::from([1, 2, 3]));
 
         let statement = Message::generate_filter_statement(&context);
-        assert_eq!(statement, " WHERE\n                     m.date >= 599558400000000000 AND     m.date <= 602323200000000000 AND     m.chat_id IN (1, 2, 3)")
+        assert_eq!(statement, " WHERE\n                     m.date >= 599558400000000000 AND     m.date <= 602323200000000000 AND     c.chat_id IN (1, 2, 3)")
     }
 
     #[test]
