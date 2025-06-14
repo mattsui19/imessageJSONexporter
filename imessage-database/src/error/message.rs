407a102e237ef54f4c13048c19dae4d0a05a3410
Christopher Sardegna <github@reagentx.net>
2025-06-14 12:39:05 -0700

    Remove unused error variants, add some new derivations

--- imessage-database/src/error/message.rs
+++ imessage-database/src/error/message.rs
@@ -4,23 +4,15 @@
 
 use std::fmt::{Display, Formatter, Result};
 
-use crate::error::{plist::PlistParseError, streamtyped::StreamTypedError};
-
-use super::typedstream::TypedStreamError;
+use crate::error::streamtyped::StreamTypedError;
 
 /// Errors that can happen when working with message table data
 #[derive(Debug)]
 pub enum MessageError {
-    /// Required data is missing from the message
-    MissingData,
     /// Message has no text content
     NoText,
     /// Error occurred when parsing with the `StreamTyped` parser
     StreamTypedParseError(StreamTypedError),
-    /// Error occurred when parsing with the `TypedStream` parser
-    TypedStreamParseError(TypedStreamError),
-    /// Error occurred when parsing Plist data
-    PlistParseError(PlistParseError),
     /// Timestamp value is invalid or out of range
     InvalidTimestamp(i64),
 }
@@ -28,7 +20,6 @@ pub enum MessageError {
 impl Display for MessageError {
     fn fmt(&self, fmt: &mut Formatter<'_>) -> Result {
         match self {
-            MessageError::MissingData => write!(fmt, "No attributedBody found!"),
             MessageError::NoText => write!(fmt, "Message has no text!"),
             MessageError::StreamTypedParseError(why) => {
                 write!(
@@ -36,15 +27,15 @@ impl Display for MessageError {
                     "Failed to parse attributedBody with legacy parser: {why}"
                 )
             }
-            MessageError::TypedStreamParseError(why) => {
-                write!(fmt, "Failed to parse attributedBody: {why}")
-            }
-            MessageError::PlistParseError(why) => {
-                write!(fmt, "Failed to parse plist data: {why}")
-            }
             MessageError::InvalidTimestamp(when) => {
                 write!(fmt, "Timestamp is invalid: {when}")
             }
         }
+    }
+}
+
+impl From<StreamTypedError> for MessageError {
+    fn from(err: StreamTypedError) -> Self {
+        MessageError::StreamTypedParseError(err)
     }
 }
